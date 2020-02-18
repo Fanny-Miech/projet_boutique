d2c26f8a7d347490a5bec82332a191e0c4d615e7
Fanny-Miech <fanny.miech@le-campus-numerique.fr>
2020-02-18 11:53:31 +0100

    requête sql

---
+++ appel_requete.php
@@ -0,0 +1,33 @@
+<?php
+include('fonction_requete_sql.php'); //appelle la page fonction
+
+//appelle la base de données 
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=bd_boutique;charset=utf8', 'fanny.miech', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+
+echo '<h3> Liste des articles dans le catalogue </h3> </br>';
+list_articles($bdd);
+
+echo '</br> </br><h3> Produits en rupture de stock </h3> </br>';
+rupture_de_stock($bdd);
+
+echo '</br> </br><h3> Liste des articles de la commande 1 </h3> </br>';
+commande_1($bdd);
+
+echo '</br> </br><h3> Liste des commandes passées par Charlize </h3> </br>';
+commandes_charlize($bdd);
+
+$client=['alibaba', 'ali.baba@40voleurs.com', 'ebjhvse', 35000, 'asialand'];
+ajouter_client($bdd, $client);
+
+suppr_client($bdd);
+
+
+?>
---
+++ fonction_requete_sql.php
@@ -0,0 +1,86 @@
+<?php
+
+//cette fonction appelle tous les articles présents dans la bdd
+function list_articles($bdd) {
+    $reponse = $bdd->query('select * from articles');
+    while ($donnees = $reponse -> fetch())
+    {
+        echo '<p>'. $donnees['name'].' - '. $donnees['price'].' euros <p>';
+    }
+}
+
+//cette fonction affiche la liste des produits en rupture de stock
+function rupture_de_stock($bdd) {
+    $reponse = $bdd->query('select * from articles where stock=0');
+    while ($donnees = $reponse -> fetch())
+    {
+        echo '<p>'. $donnees['name'].' - '. $donnees['price'].' euros <p>';
+    }
+}
+
+//cette fonction affiche les articles de la commande 1
+function commande_1($bdd) {
+    $reponse = $bdd->query('SELECT articles.name, articles.price, articles_commandes.quantity 
+    FROM articles 
+    INNER JOIN articles_commandes ON articles_commandes.Articles_id=articles.id
+    WHERE Orders_id=1');
+    while ($donnees = $reponse -> fetch()) {
+        echo '<p>'. $donnees['name'].' - '. $donnees['price'].' euros'.' - '.$donnees['quantity'].'<p>';
+    }
+}
+
+//cette fonction affiche les commandes passées par Charlize
+function commandes_charlize($bdd) {
+    $reponse = $bdd->query('SELECT orders.* 
+    From orders 
+    INNER JOIN users ON orders.Users_id=users.id
+    WHERE users.name LIKE \'%Charlize%\'');
+    while ($donnees = $reponse -> fetch()) {
+    echo '<p> Commande n° '. $donnees['numero'].'<p>';
+    }
+}
+
+//cette fonction supprime les clients qui n'ont pas de commande
+function suppr_client($bdd) {
+    $reponse = $bdd->query('DELETE FROM users
+    WHERE users.id 
+    NOT IN (
+        SELECT orders.Users_id
+        FROM orders
+        )
+    ');
+}
+
+//cette fonction ajoute un article
+function add_article($bdd, $articles) {
+    $req = $bdd->prepare('INSERT INTO articles(name, description, price, poids, image, stock, for_sale, Categories_id) 
+    VALUES(:name, :description, :price, :poids, :image, :stock, :for_sale, :Categories_id)');
+    $req->execute(array(
+        'name' => $articles[0],
+        'description' => $articles[1],
+        'price' => $articles[2],
+        'poids' => $articles[3],
+        'image' => $articles[4],
+        'stock' => $articles[5],
+        'for_sale' => $articles[6],
+        'Categories_id' => $articles[7]
+        ));
+
+    echo "L'article ".$articles[0]." a bien été ajouté !";
+}
+
+//cette fonction ajoute un client
+function ajouter_client($bdd, $client) {
+    $req = $bdd->prepare('INSERT INTO users(name, email, adress, postal_code, city) 
+    VALUES(:name, :email, :adress, :postal_code, :city)');
+    $req->execute(array(
+        'name' => $client[0],
+        'description' => $client[1],
+        'price' => $client[2],
+        'poids' => $client[3],
+        'image' => $client[4]
+        ));
+
+    echo "Le client ".$articles[0]." a bien été ajouté !";
+}
+?>
