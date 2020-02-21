660048487b28ca53cec7820dc29d7e5b9437b14b
Fanny-Miech <fanny.miech@le-campus-numerique.fr>
2020-02-21 16:21:29 +0100

    déplacement des fichiers inutiles vers archives/

---
+++ archives/index.php
@@ -0,0 +1,39 @@
+<?php 
+session_start();
+include ("fonctions.php");//appelle la page fonction
+
+//définition des variables : les variables sont des tableaux de strings
+$art1 = ["Grande muraille de Chine","1000 €","images/muraille_de_chine.jpg"];
+$art2 = ["Machu Picchu","1200 €","images/machu_picchu.jpg" ];
+$art3 = ["Taj Mahal", "1300 €", "images/taj_mahal.jpg"];
+
+$catalogue = [$art1, $art2, $art3];
+
+include ("entete.php"); //appelle la page d'entete
+?>
+
+
+<header>
+<h1>Les merveilles du monde</h1>
+</header>
+
+<div>
+<!-- crée un formulaire avec les différents articles du catalogue-->
+
+<form method="post" action="panierV3.php">
+<?php 
+foreach ($catalogue as $article) {?>
+<div>
+
+<input type="checkbox" name="<?php echo $article[0] ?>" id="<?php echo $article[0] ?>" />Sélectionner l'article</input>
+<label for="<?php echo $article[0] ?>"><?php afficheArticle ($article[0],$article[2],$article[1]); }?></label>
+
+</div>
+
+<input type="submit" value="Ajouter au panier" />
+<form>
+</div>
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,38 +2,56 @@
 session_start();
 include ("fonctions.php");//appelle la page fonction
 
-//définition des variables : les variables sont des tableaux de strings
-$art1 = ["Grande muraille de Chine","1000 €","images/muraille_de_chine.jpg"];
-$art2 = ["Machu Picchu","1200 €","images/machu_picchu.jpg" ];
-$art3 = ["Taj Mahal", "1300 €", "images/taj_mahal.jpg"];
-
-$catalogue = [$art1, $art2, $art3];
+//appelle la base de données 
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=bd_boutique;charset=utf8', 'fanny.miech', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
 
 include ("entete.php"); //appelle la page d'entete
 ?>
 
 
 <header>
-<h1>Les merveilles du monde</h1>
+<h1>Notre catalogue</h1>
 </header>
 
 <div>
 <!-- crée un formulaire avec les différents articles du catalogue-->
 
-<form method="post" action="panierV3.php">
+<form method="post" action="panierV4_bdd.php">
 <?php 
-foreach ($catalogue as $article) {?>
-<div>
 
-<input type="checkbox" name="<?php echo $article[0] ?>" id="<?php echo $article[0] ?>" />Sélectionner l'article</input>
-<label for="<?php echo $article[0] ?>"><?php afficheArticle ($article[0],$article[2],$article[1]); }?></label>
+$reponse = $bdd->query('select * from articles');
+    while ($donnees = $reponse -> fetch())
+    {?>
+        <div class="card-formulaire">
+      
+    <label for="<?php echo $donnees['id'] ?>"><?php afficheArticle($donnees['name'], $donnees['image'], $donnees['price']);?>
+        <?php if ($donnees['stock']==0) {
+        echo '<h3 class="indisponible"> Cet article est indisponible </h3>';
+    }
+    else {?>
+        <input class="checkbox" type="checkbox" name="<?php echo $donnees['id'] ?>" id="<?php echo $donnees['id'] ?>" />Sélectionner l'article</input>
+    <?php
+    }
+    echo '<br /><br /><HR>';
+}?>
+        </label>
+        <br/><br/>
+        </div>
 
-</div>
+        <input class="btn-lg" type="submit" value="Ajouter au panier" />
+        <form>
+        </div>
+</br></br></br>
 
-<input type="submit" value="Ajouter au panier" />
-<form>
-</div>
-
-
-</body>
+        </body>
+<?php
+include ("footer.php");
+?>
 </html>
