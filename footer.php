7419f657b3750ea20ccdd8516256402e2cb7220c
Fanny-Miech <fanny.miech@le-campus-numerique.fr>
2020-02-21 14:26:02 +0100

    +footer

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+<!-- Footer -->
+<footer class="footer">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">In the Air</h5>
+        <p>Des rêves à portée de main</p>
+    </div>
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div>© 2020 All rights reserved
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
